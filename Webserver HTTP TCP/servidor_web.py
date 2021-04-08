2dc14571d40dddd81e9725a8d424e75d4981d39b
Gustavo Nascimento <56653979+Gustanascimento@users.noreply.github.com>
2021-04-08 00:13:29 -0300

    While True

--- Webserver HTTP TCP/servidor_web.py
+++ Webserver HTTP TCP/servidor_web.py
@@ -1,29 +1,34 @@
 from socket import socket, AF_INET, SOCK_STREAM
 
 server_socket = socket(AF_INET, SOCK_STREAM)
-
 server_socket.bind(('localhost', 9000))
 
-server_socket.listen()
 
-client_socket, address_client = server_socket.accept()
+while True:
+    #Recebe Requisição HTTP
+    server_socket.listen()
+    client_socket, address_client = server_socket.accept()
+    data = client_socket.recv(2048)
+    dados = data.decode()
 
-data = client_socket.recv(2048)
+    print(f'{dados}') #Dados recebidos
 
-print(f'{data.decode()}')
+    #Criação da Resposta
 
-msg = ('HTTP/1.1 200 OK\r\n'
-       'Date: Thu, 24 Sep 2020 21:00:15 GMT\r\n'
-       'Server: Dummy/0.0.1 (Ubuntu)\r\n'
-       'Content-Type: text/html\r\n'
-       '\r\n')
+    msg = ('HTTP/1.1 200 OK\r\n'
+           'Date: Thu, 24 Sep 2020 21:00:15 GMT\r\n'
+           'Server: Dummy/0.0.1 (Ubuntu)\r\n'
+           'Content-Type: text/html\r\n'
+           '\r\n')
 
-msg += ('<html><head><title>Eu sou um html de exemplo</title></head>'
-        '<body><h1>Eu sou uma pagina de exemplo</h1>'
-        '<h3>Eu sou um titulo menor</h3>'
-        '</body>'
-        '</html>')
+    msg += ('<html><head><title>Eu sou um html de exemplo</title></head>'
+            '<body><h1>Eu sou uma pagina de exemplo</h1>'
+            '<h3>Eu sou um titulo menor</h3>'
+            '</body>'
+            '</html>')
 
-client_socket.send(msg.encode())
+    client_socket.send(msg.encode())
 
-client_socket.close()
+    client_socket.close()
+
+    print("FIM DA REQUISICAO\n")
